c3f768031831a546c59c3cc16b89dbc2345f1751
dgonrom2803 <dgonrom2803@g.educaand.es>
2022-11-03 14:33:39 +0100

    Create EjemploWhile.java

---
+++ EjemploWhile.java
@@ -0,0 +1,9 @@
+public class EjemploWhile {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i >= 0) {
+            System.out.println("i: " + i);
+            i--;
+        }
+    }
+}
